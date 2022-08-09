3597b6ff6e12cc2c2ce6e99bd20e9af4337a0fe9
hamdi <almawryhmdyjmal@gmail.com>
2022-08-09 11:45:46 +0300

    Initial commit

---
+++ test.py
@@ -0,0 +1,33 @@
+c=0
+snum="10^2"
+for i in snum:
+    if i=='^':
+        a=int(snum[c-2] + snum[c-1])
+        print(a)
+    c=c+1
+    #     for x in range(int(snum[c+1])):
+    #         b=b*a
+    #     t=True
+    # c=c+1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
